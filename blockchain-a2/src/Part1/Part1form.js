c9baf5b29a60c4d093be3ee39cb87896723344cc
SefaSoylu <s3794642@student.rmit.edu.au>
2025-04-28 00:12:49 +1000

    forf for adding a new record

--- blockchain-a2/src/Part1/Part1form.js
+++ blockchain-a2/src/Part1/Part1form.js
@@ -1,8 +1,16 @@
 function Part1form(){
     return(
-        <form>
-            <label for='p'>p</label><br></br>
-            <input type='text' id='p' name='p'></input><br></br>
+        <form> 
+            <h1>Add a new record</h1>
+            <label for='id'>Item ID:</label>
+            <input type='number' id='id' name='id'></input>
+            <label for='qty'>Item QTY:</label>
+            <input type='number' id='qty' name='qty'></input>
+            <label for='price'>Item Price:</label>
+            <input type='number' id='price' name='price'></input>
+            <label for='location'>Location:</label>
+            <input type='text' id='location' name='location'></input>
+            <input type="submit" value="Add new Record"></input>
         </form>
     )
 }
